f051255146dd810744be6fbc80e5db9b55ef41cb
Sujeet17m <sujeet17das@gmail.com>
2025-05-29 17:46:45 +0530

    50%of challenge completed

--- myApp.js
+++ myApp.js
@@ -1,5 +1,24 @@
+const { config } = require('dotenv');
 let express = require('express');
 let app = express();
+require('dotenv').config();
+
+console.log("Hello World");
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname+'/views/index.html');
+});
+
+app.use('/public',express.static(__dirname + '/public'))
+app.get('/json',function(req,res){
+    let message = "Hello json";
+    if(process.env.MESSAGE_STYLE === 'uppercase'){
+        message = message.toUpperCase();
+    }
+    res.json({message: message});
+});
+
+
 
 
 
